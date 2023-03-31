45e71bf05dc75f890d471dcb2d28ad4ef2560815
Chuck Connell <chuck.connell.3@gmail.com>
2023-03-31 18:28:12 -0400

    Full working version with WHO data added in

--- make_gh_covid_summary.py
+++ make_gh_covid_summary.py
@@ -7,9 +7,10 @@
 import os
 import fnmatch
 import pandas as pd 
+import datetime as dt
 
 WHO_COVID_FILE = "https://covid19.who.int/WHO-COVID-19-global-data.csv"
-GH_COUNTRY_DIR = "/Users/chuck/Desktop/COVID Programming/Global.Health/gh_2023-03-30/country/"
+GH_COUNTRY_DIR = "/Users/chuck/Desktop/COVID Programming/Global.Health/gh_2023-03-30/country_test/"
 GH_SUMMARY_FILE = "gh_summary.tsv"
 
 # Tell user what set of G.h files we are looking at. 
@@ -20,18 +21,19 @@ print ("\nWorking in directory: " + GH_COUNTRY_DIR)
 # Get WHO data about cases and deaths per country. 
 # Throw out colummns we don't need. Clarify some field naming. Keep just the latest data.
 
-who_DF = pd.read_csv(WHO_COVID_FILE, sep=',', header='infer', dtype=str, nrows=1000)
-#who_DF = pd.read_csv(WHO_COVID_FILE, sep=',', header='infer', dtype=str)
+who_DF = pd.read_csv(WHO_COVID_FILE, sep=',', header='infer', dtype=str)
 
 who_DF = who_DF[["Date_reported", "Country_code", "Cumulative_cases", "Cumulative_deaths"]] 
 who_DF = who_DF.rename({"Date_reported":"who_Date_reported", "Cumulative_cases":"who_Cumulative_cases", "Cumulative_deaths":"who_Cumulative_deaths"}, axis='columns')
 
+who_DF["who_Date_reported"] = pd.to_datetime(who_DF["who_Date_reported"], infer_datetime_format=True)
 latest_who = who_DF["who_Date_reported"].max()
 who_DF = who_DF.loc[who_DF["who_Date_reported"] == latest_who]
 
 # Make a dataframe that will hold the output.
 
-summary_DF = pd.DataFrame(data=None, dtype=str, columns=["country", "gh_latest_case", "gh_cases", "gh_hospital_yes", "gh_icu_yes", "gh_outcome_admit", "gh_outcome_icu", "gh_outcome_death"])
+summary_DF = pd.DataFrame()
+#summary_DF = pd.DataFrame(data=None, dtype=str, columns=["country", "gh_latest_case", "gh_cases", "gh_hospital_yes", "gh_icu_yes", "gh_outcome_admit", "gh_outcome_icu", "gh_outcome_death"])
 
 # Loop over all the files in the input directory.
  
@@ -53,10 +55,11 @@ for f in files:
 
     # Find the number of rows and last date.
     
-    gh_DF = pd.read_csv(gh_path, sep=',', header='infer', dtype=str, nrows=1000)
-    #gh_DF = pd.read_csv(gh_path, sep=',', header='infer', dtype=str)
-    gh_rows = str(gh_DF.shape[0])
-    gh_latest = str(gh_DF["events.confirmed.date"].max())
+    gh_DF = pd.read_csv(gh_path, sep=',', header='infer', dtype=str)
+    gh_rows = gh_DF.shape[0]
+
+    gh_DF["events.confirmed.date"] = pd.to_datetime(gh_DF["events.confirmed.date"], infer_datetime_format=True)
+    gh_latest = gh_DF["events.confirmed.date"].max()
 
     # Lowercase the fields we care about, just to prevent upper/lower issues
 
@@ -80,8 +83,12 @@ for f in files:
     icu_yes = icus.get("yes", 0)
     
     # Append info for this file to the overall output spreadsheet.
-    
-    summary_DF = summary_DF.append({"country":country, "gh_latest_case":gh_latest, "gh_cases":gh_rows, "gh_hospital_yes":hospital_yes, "gh_icu_yes":icu_yes, "gh_outcome_admit":outcome_admit, "gh_outcome_icu":outcome_icu, "gh_outcome_death":outcome_death}, ignore_index=True)
+    #summary_DF = summary_DF.append({"country":country, "gh_latest_case":gh_latest, "gh_cases":gh_rows, "gh_hospital_yes":hospital_yes, "gh_icu_yes":icu_yes, "gh_outcome_admit":outcome_admit, "gh_outcome_icu":outcome_icu, "gh_outcome_death":outcome_death}, ignore_index=True)
+
+    # TODO change this to concat([df1, df2])
+    # this_row_DF = pd.DataFrame({'a': [1, 2], 'b': [3, 4], 'c': [5, 6]})
+    this_country_DF = pd.DataFrame({"country":[country], "gh_latest_case":[gh_latest], "gh_cases":[gh_rows], "gh_hospital_yes":[hospital_yes], "gh_icu_yes":[icu_yes], "gh_outcome_admit":[outcome_admit], "gh_outcome_icu":[outcome_icu], "gh_outcome_death":[outcome_death]})
+    summary_DF = pd.concat([summary_DF, this_country_DF])
     
 # Done with file loop. Close the file list.
 
@@ -92,9 +99,28 @@ files.close()
 summary_DF = summary_DF.merge(who_DF, how='left', left_on="country", right_on = "Country_code")
 summary_DF = summary_DF.drop(columns=["Country_code"])
 
-# Calc some columns based on the G.h data and other data for the countries.
+# Calc the percent of WHO cases that G.h has for each country
 
-# TODO  Cumulative_cases, Cumulative_deaths
+summary_DF["gh_cases"] = summary_DF["gh_cases"].fillna(0).astype(int)
+summary_DF["who_Cumulative_cases"] = summary_DF["who_Cumulative_cases"].fillna(0).astype(int)
+
+summary_DF["gh_v_who_cases"] = ((summary_DF["gh_cases"] / summary_DF["who_Cumulative_cases"]) * 100).round()
+
+# Calc pct of WHO deaths that G.h has for each country.
+
+summary_DF["gh_outcome_death"] = summary_DF["gh_outcome_death"].fillna(0).astype(int)
+summary_DF["who_Cumulative_deaths"] = summary_DF["who_Cumulative_deaths"].fillna(0).astype(int)
+
+summary_DF["gh_v_who_deaths"] = ((summary_DF["gh_outcome_death"] / summary_DF["who_Cumulative_deaths"]) * 100).round()
+
+# Calc overall mortality percent from G.h cases for each country.
+
+summary_DF["gh_mortality"] = ((summary_DF["gh_outcome_death"] / summary_DF["gh_cases"]) * 100).round(1)
+
+# How old is the latest G.h data for each country?
+
+today = dt.date.today()
+summary_DF["gh_data_age"] = (today - summary_DF["gh_latest_case"].dt.date).dt.days
 
 # Write the spreadsheet.
 
